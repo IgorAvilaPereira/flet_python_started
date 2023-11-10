342158a9ec7ab5c65b48a0bb5da1123eb5c1cee7
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-10 18:18:51 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,21 @@
+import flet as ft
+
+def main(page: ft.Page):
+    t = ft.Text(value="Hello, world!", color="green")
+    page.controls.append(t)
+    page.update()
+
+# desktop
+ft.app(target=main)
+
+# web
+# ft.app(target=main, view="web_browser")
+
+
+
+
+
+
+
+
+
