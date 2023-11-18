19f258989664adb31861911f5f2df52caf329983
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2023-11-18 10:29:54 -0300

    ok

--- teste.py
+++ teste.py
@@ -5,12 +5,18 @@ def main(page: ft.Page):
     page.controls.append(t)
     page.update()
 
+
 # desktop
 ft.app(target=main)
+# no terminal: python teste.py
+
+# android
+# ft.app(target=main)
+# no terminal: flet run teste.py
 
 # web
 # ft.app(target=main, view="web_browser")
-
+# no terminal: python teste.py
 
 
 
